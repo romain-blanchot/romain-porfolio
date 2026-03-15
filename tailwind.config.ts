82685d8294d80f709c2a072d2ed729ad7e58fb96
Romain Blanchot <blanchot@et.esiea.fr>
2026-03-15 19:40:17 +0100

    chore: add @next/eslint-plugin-next and update tailwind content paths

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,9 +3,7 @@ import type { Config } from "tailwindcss";
 export default {
     darkMode: ["class"],
     content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
   	extend: {
